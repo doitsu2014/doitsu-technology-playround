bd8cc6967804bc1cae0ef70c985d46cb6ad4952d
Duc Tran <thd1152015@gmail.com>
2022-03-27 21:14:34 +0700

    elasticsearch and redis (#4)
    * Utilities
    
    * update
    
    * fuck
    
    * Update
    
    * Update
    
    * update
    
    * Update
    
    * Update
    
    Co-authored-by: Duc Tran <huuduc.tran@eitsvn.com>

---
+++ kubernetes/redis/install.sh
@@ -0,0 +1,7 @@
+NAMESPACE=redis
+SERVER_NAME=redis-server
+
+helm repo add bitnami https://charts.bitnami.com/bitnami
+kubectl create namespace ${NAMESPACE}
+helm install ${SERVER_NAME} -f global-value.yml bitnami/redis --namespace ${NAMESPACE}
+kubectl apply -f ingress.yml --namespace=${NAMESPACE}
