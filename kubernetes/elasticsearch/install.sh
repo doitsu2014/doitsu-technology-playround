1a55bf9251470f3290eadbea5a7938aab9ca4a49
Doitsu Technology - Duc TH <thd1152015@gmail.com>
2022-03-31 11:27:44 +0700

    Update

--- kubernetes/elasticsearch/install.sh
+++ kubernetes/elasticsearch/install.sh
@@ -1,5 +1,6 @@
 SERVER_NAME=elasticsearch
 KIBANA_NAME=elasticsearch-kibana
+ECK_NAME=elasticsearch-eck
 NAMESPACE=elasticsearch
 ELASTIC_PASSWORD=zaQ@1234
 
@@ -7,7 +8,6 @@ helm repo add elastic https://helm.elastic.co
 kubectl create namespace ${NAMESPACE}
 
 helm install ${SERVER_NAME} -f elasticsearch-default-value.yml elastic/elasticsearch --version 7.17.1 --namespace=${NAMESPACE}
-kubectl create secret generic elastic-credentials --from-literal=password={ELASTIC_PASSWORD} --from-literal=username=elastic
 kubectl apply -f ingress.yml --namespace=${NAMESPACE}
 
 helm install ${KIBANA_NAME} -f elasticsearch-kibana-default-value.yml elastic/kibana --version 7.17.1 --namespace=${NAMESPACE}
