c544c6590dec986763525a533238ac8b0007cf07
eTreem - Duc TH <dtran@etreem.com>
2022-04-03 18:02:39 +0700

    aaa

--- aks/redis/update.sh
+++ aks/redis/update.sh
@@ -3,4 +3,4 @@ SERVER_NAME=redis-server
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
 kubectl create namespace ${NAMESPACE}
-helm install ${SERVER_NAME} -f global-value.yml bitnami/redis --namespace ${NAMESPACE}
+helm upgrade ${SERVER_NAME} -f global-value.yml bitnami/redis --namespace ${NAMESPACE}
