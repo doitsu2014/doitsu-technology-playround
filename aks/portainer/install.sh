22bdcf3ae4882414c1e05c89f49fac9967efaa1e
Duc Tran <thd1152015@gmail.com>
2022-05-09 09:46:32 +0700

    Update

--- aks/portainer/install.sh
+++ aks/portainer/install.sh
@@ -3,5 +3,5 @@ NAMESPACE=portainer
 kubectl create namespace ${NAMESPACE}
 helm repo add portainer https://portainer.github.io/k8s/
 helm repo update
-helm install -n ${NAMESPACE} portainer portainer/portainer --set service.type=ClusterIP
+helm install -n ${NAMESPACE} portainer portainer/portainer --set service.type=NodePort
 kubectl apply ingress.yml --namespace=${NAMESPACE}
