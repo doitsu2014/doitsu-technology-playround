24a3dc79f0bc44af5ca09daa982edb68502c6a99
Duc Tran <thd1152015@gmail.com>
2022-05-09 10:51:54 +0700

    Update (#10)

--- kubernetes/infrastructure-localhost/install-mssql.sh
+++ kubernetes/infrastructure-localhost/install-mssql.sh
@@ -11,4 +11,5 @@ kubectl config set-context --current --namespace=$NAMESPACE
 
 helm repo add mssql-server-2019 https://simcubeltd.github.io/mssql-server-helm/charts/
 
+helm uninstall my-mssql-linux -n ${NAMESPACE} 
 helm install my-mssql-linux mssql-server-2019/mssql-linux -f mssql/mssql-default-value.yml --namespace=${NAMESPACE} 
