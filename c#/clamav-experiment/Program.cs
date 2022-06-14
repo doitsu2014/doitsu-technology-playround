77d71074f6591c8d3ad7b7fbb1f69b0405aacd39
Doitsu Technology - Duc TH <thd1152015@gmail.com>
2022-06-14 16:03:32 +0700

    Auto stash before checking out "origin/main"

---
+++ c#/clamav-experiment/Program.cs
@@ -0,0 +1,23 @@
+﻿using nClam;
+
+var clamClient   = new ClamClient("localhost", 3310);
+var text         = await File.ReadAllTextAsync("./fiinpro_sessions.saz");
+var memoryStream = new MemoryStream();
+var streamWriter = new StreamWriter(memoryStream);
+await streamWriter.WriteAsync(text);
+
+var result = await clamClient.SendAndScanFileAsync(memoryStream);
+
+streamWriter.Close();
+memoryStream.Flush();
+memoryStream.Close();
+
+switch (result.Result)
+{
+    case ClamScanResults.Clean:
+        Console.WriteLine("File is clean");
+        break;
+    default:
+        Console.WriteLine("File is not clean");
+        break;
+}
