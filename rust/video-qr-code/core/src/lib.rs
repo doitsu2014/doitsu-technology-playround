80d425be163566c802db07a3c5ba3656576925d4
Huu Duc Tran <thd1152018@gmail.com>
2022-07-10 15:29:57 +0700

    Update interested thing

--- rust/video-qr-code/core/src/lib.rs
+++ rust/video-qr-code/core/src/lib.rs
@@ -6,68 +6,82 @@ use std::{
 
 struct Configuration {
     is_debug: bool,
-    speed_up: f64,
 }
 
-pub fn get_qr_codes_from_video(video_file_path: &str) -> Result<HashMap<u64, String>> {
-    let configuration = Configuration { is_debug: false, speed_up: 0.25 };
+pub fn get_qr_codes_from_video(video_file_path: &str, period: f64) -> Result<HashMap<u64, String>> {
+    let configuration = Configuration { is_debug: true };
+
     let mut qr_detector = objdetect::QRCodeDetector::default()?;
     let mut res = types::VectorOfPoint::new();
     // let mut capture = videoio::VideoCapture::from_file(video_file_path, videoio::CAP_PROP_FRAME_COUNT)?;
 
-
     println!("loading video path: {}", video_file_path);
     let mut camera = videoio::VideoCapture::from_file(video_file_path, videoio::CAP_ANY)?;
-    let frames = camera.get(videoio::CAP_PROP_FRAME_COUNT)?;
+
+    let total_of_frames = camera.get(videoio::CAP_PROP_FRAME_COUNT)?;
     let fps = camera.get(videoio::CAP_PROP_FPS)?;
-    println!("fps: {}", fps);
-    let duration = frames/fps;
-    println!("Duration (s): {}", duration);
-    let new_fps= fps + (fps * 0.25);
-    let result_setting_new_fps = camera.set(videoio::CAP_PROP_FPS, new_fps)?;
-    println!("Setting new fps result: {}", result_setting_new_fps);
+    let duration = total_of_frames / fps;
+    let number_of_parts_by_period = (duration / period).round() as u64;
+    let number_of_frames_in_period = (fps * period).round() as u64;
+    println!("Capturing Video's duration: {}", duration);
+    println!("Period: {} (s)", period);
+    println!(
+        "Number of parts of capturing video when it is divided in period: {}",
+        number_of_parts_by_period
+    );
+    println!(
+        "Number of frames in period: {}",
+        number_of_frames_in_period
+    );
 
     let mut img = Mat::default();
     let mut recqr = Mat::default();
     let mut qr_codes: HashMap<u64, String> = HashMap::new();
 
-    loop {
-        let camera_read_result = camera.read(&mut img)?;
-        if camera_read_result == false {
-            // let err = camera_read_result.unwrap_err().message;
-            println!("End of video");
-            break;
-        }
-        let ret = qr_detector.detect_and_decode(&img, &mut res, &mut recqr)?;
-        let s = String::from_utf8_lossy(&ret);
-        if !s.is_empty() {
-            let calculated_hash = calculate_hash(&s.to_string());
-            if !qr_codes.contains_key(&calculated_hash) {
-                println!("find a qr code {}", s);
-                qr_codes.insert(calculated_hash, s.to_string());
+    for part in 0..number_of_parts_by_period {
+        println!("Capturing part {}", part);
+        let begin_index = (part * number_of_frames_in_period);
+        camera.set(videoio::CAP_PROP_POS_FRAMES, begin_index as f64)?;
+        for i in 0..number_of_frames_in_period {
+            camera.read(&mut img)?;
+            let ret = qr_detector.detect_and_decode(&img, &mut res, &mut recqr)?;
+            let s = String::from_utf8_lossy(&ret);
+            if !s.is_empty() {
+                let calculated_hash = calculate_hash(&s.to_string());
+                if !qr_codes.contains_key(&calculated_hash) {
+                    println!("find a qr code {}", s);
+                    qr_codes.insert(calculated_hash, s.to_string());
+                }
             }
-        }
 
-        if configuration.is_debug {
-            highgui::named_window("QR Code", highgui::WINDOW_NORMAL)?;
-            if recqr.size()?.width > 0 {
-                highgui::imshow("QR Code", &recqr)?;
+            if configuration.is_debug {
+                let winname = "QR Code";
+                highgui::named_window(winname, highgui::WINDOW_NORMAL)?;
+                if recqr.size()?.width > 0 {
+                    highgui::imshow(winname, &recqr)?;
+                }
+                if res.len() > 0 {
+                    imgproc::polylines(
+                        &mut img,
+                        &res,
+                        true,
+                        core::Scalar::new(0f64, 255f64, 0f64, 0f64),
+                        1,
+                        1,
+                        0,
+                    )?;
+                }
+
+                highgui::imshow(winname, &img)?;
+                let key = highgui::wait_key(1)?;
+                if key == 'q' as i32 {
+                    break;
+                }
             }
-            if res.len() > 0 {
-                imgproc::polylines(
-                    &mut img,
-                    &res,
-                    true,
-                    core::Scalar::new(0f64, 255f64, 0f64, 0f64),
-                    1,
-                    1,
-                    0,
-                )?;
-            }
-            highgui::imshow("Frame", &img)?;
         }
     }
 
+    println!("End of capture task");
     Ok(qr_codes)
 }
 
--- rust/video-qr-code/src/main.rs
+++ rust/video-qr-code/src/main.rs
@@ -1,11 +1,12 @@
 use core::*;
 
 pub fn main() {
-    let result = get_qr_codes_from_video("/Users/doitsu2014/Workspace/doitsu-technology/videos/IMG_5576.mov").unwrap();
+    // Concurrency Fearless
+    let result = get_qr_codes_from_video("/Users/doitsu2014/Workspace/doitsu-technology/videos/IMG_5576.mov", 30.0).unwrap();
 
-    // Print the HashMap of Qr Code Values
-    result.into_iter().for_each(|x| {
-        println!("Browsing qr code: {}", x.0);
-        println!("Values: {}", x.1);
-    });
+    // // Print the HashMap of Qr Code Values
+    // result.into_iter().for_each(|x| {
+    //     println!("Browsing qr code: {}", x.0);
+    //     println!("Values: {}", x.1);
+    // });
 }
